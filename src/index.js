2db7b4a6d2c0e2238f479db8af5df77d4e790b91
mailer58 <50555861+mailer58@users.noreply.github.com>
2020-02-20 03:46:29 +0300

    Add task 7

--- src/index.js
+++ src/index.js
@@ -8,10 +8,94 @@ const FilmData = {
   YEAR: 2014
 };
 
+const filmsCards = [{
+  title: `Fantastic Beasts: The Crimes of Grindelwald`,
+  img: `img/snatch.jpg`
+},
+{
+  title: `Bohemian Rhapsody`,
+  img: `img/snatch.jpg`
+},
+{
+  title: `Macbeth`,
+  img: `img/snatch.jpg`
+},
+{
+  title: `Aviator`,
+  img: `img/snatch.jpg`
+},
+{
+  title: `We need to talk about Kevin`,
+  img: `img/snatch.jpg`
+},
+{
+  title: `What We Do in the Shadows`,
+  img: `img/snatch.jpg`
+},
+{
+  title: `Revenant`,
+  img: `img/snatch.jpg`
+},
+{
+  title: `Johnny English`,
+  img: `img/snatch.jpg`
+}, {
+  title: `Shutter Island`,
+  img: `img/snatch.jpg`
+}, {
+  title: `Pulp Fiction`,
+  img: `img/snatch.jpg`
+}, {
+  title: `No Country for Old Men`,
+  img: `img/snatch.jpg`
+}, {
+  title: `Snatch`,
+  img: `img/snatch.jpg`
+}, {
+  title: `Moonrise Kingdom`,
+  img: `img/snatch.jpg`
+},
+{
+  title: `Seven Years in Tibet`,
+  img: `img/snatch.jpg`
+},
+{
+  title: `Midnight Special`,
+  img: `img/snatch.jpg`
+},
+{
+  title: `War of the Worlds`,
+  img: `img/snatch.jpg`
+},
+{
+  title: `Dardjeeling Limited`,
+  img: `img/snatch.jpg`
+},
+{
+  title: `Orlando`,
+  img: `img/snatch.jpg`
+},
+{
+  title: `Mindhunter`,
+  img: `img/snatch.jpg`
+},
+{
+  title: `Midnight Special`,
+  img: `img/snatch.jpg`
+},
+];
+
 ReactDOM.render(<
-  App title = {FilmData.TITLE}
-  genre = {FilmData.GENRE}
-  year = {FilmData.YEAR}
+  App title = {
+    FilmData.TITLE
+  }
+  genre = {
+    FilmData.GENRE
+  }
+  year = {
+    FilmData.YEAR
+  }
+  cards = {filmsCards}
 />,
 document.querySelector(`#root`)
 );
