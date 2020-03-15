cd54ac31e7eeb520cd24644eb49f1c6c40b3e9a8
mailer58 <50555861+mailer58@users.noreply.github.com>
2020-03-15 04:32:59 +0300

    Fix icon of My list button

--- src/components/movie-details/movie-details.jsx
+++ src/components/movie-details/movie-details.jsx
@@ -25,7 +25,9 @@ export const MovieDetails = (props) => {
           </div>
 
           <h1 className="visually-hidden">WTW</h1>
+
           <PageHeader />
+
           <div className="movie-card__wrap">
             <div className="movie-card__desc">
               <h2 className="movie-card__title">{title}</h2>
@@ -37,13 +39,13 @@ export const MovieDetails = (props) => {
               <div className="movie-card__buttons">
                 <button className="btn btn--play movie-card__button" type="button">
                   <svg viewBox="0 0 19 19" width="19" height="19">
-                    <use xmlnsXlink="#play-s"></use>
+                    <use xlinkHref="#play-s"></use>
                   </svg>
                   <span>Play</span>
                 </button>
                 <button className="btn btn--list movie-card__button" type="button">
                   <svg viewBox="0 0 19 20" width="19" height="20">
-                    <use xmlnsXlink="#add"></use>
+                    <use xlinkHref="#add"></use>
                   </svg>
                   <span>My list</span>
                 </button>
--- src/components/movie-overview/movie-overview.jsx
+++ src/components/movie-overview/movie-overview.jsx
@@ -48,7 +48,9 @@ export const MovieOverview = ({film, renderScreens, tab}) => {
           </div>
 
           <h1 className="visually-hidden">WTW</h1>
+
           <PageHeader />
+
           <div className="movie-card__wrap">
             <div className="movie-card__desc">
               <h2 className="movie-card__title">{title}</h2>
@@ -60,13 +62,13 @@ export const MovieOverview = ({film, renderScreens, tab}) => {
               <div className="movie-card__buttons">
                 <button className="btn btn--play movie-card__button" type="button">
                   <svg viewBox="0 0 19 19" width="19" height="19">
-                    <use xmlnsXlink="#play-s"></use>
+                    <use xlinkHref="#play-s"></use>
                   </svg>
                   <span>Play</span>
                 </button>
                 <button className="btn btn--list movie-card__button" type="button">
                   <svg viewBox="0 0 19 20" width="19" height="20">
-                    <use xmlnsXlink="#add"></use>
+                    <use xlinkHref="#add"></use>
                   </svg>
                   <span>My list</span>
                 </button>
